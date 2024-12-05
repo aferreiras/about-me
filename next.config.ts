b2fb006d0f15c0cf413bd8c0228f0b38d288250b
aferreiras <9977792+aferreiras@users.noreply.github.com>
2024-12-04 21:42:42 -0300

    lock and nextconfig

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   output: "export",
+  assetPrefix: isProd ? 'https://github.com/aferreiras/about-me' : '',
+  basePath: isProd ? '/docs' : '',
+  images: {
+    unoptimized: true,
+  },
   compiler: {
     styledComponents: true,
   }
